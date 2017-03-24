ee4ee6f7a0dabe218b6bf31549ce03617db04c45
Steve <urgent.programmer@gmail.com>
2017-03-23 22:42:16 -0400

    Server -- jsdom include
    Added jsdom npm for use to transform html generated by neutrino build.
    
    Changed indents, added comments to server/index.js

--- server/index.js
+++ server/index.js
@@ -2,22 +2,27 @@ import express from 'express';
 import React from "react";
 import { renderToString } from 'react-dom/server';
 import site from '../src/index.js';
+import jsdom from "jsdom";
 
 const fs = require('fs');
 
+// Load neutrino build with filesystem
 fs.readFile('build/index.html', 'utf8', function (err, html) {
-  console.log(err);
-  console.log(html);
-  var app = express();
+  // parse html file into a DOM to allow for selectors, append, prepend
+  jsdom.env(html, function(err, window) {
+    // apply site transforms, an array of callbacks that contain
 
-  app.use(express.static(process.cwd() + '/build'));
+    // start serving express
+    var app = express();
+    app.use(express.static(process.cwd() + '/build'));
 
-  app.get('/', function (req, res) {
-    const appString = renderToString(site.hello);
-    res.send(html);
-  })
+    app.get('/', function (req, res) {
+      const appString = renderToString(site.hello);
+      res.send(html);
+    })
 
-  app.listen(3000, function () {
-    console.log('SSR listening on port 3000!')
-  })
+    app.listen(3000, function () {
+      console.log('SSR listening on port 3000!')
+    })
+  });
 });
